70c976261eaeacc7f13f864b064d9346392d299b
zhangpeng <13761760@qq.com>
2015-06-26 00:26:05 +0800

    secend commit

--- LearningGit/AppDelegate.m
+++ LearningGit/AppDelegate.m
@@ -18,7 +18,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"test");
-
+ NSLog(@"test");
     return YES;
 }
 
